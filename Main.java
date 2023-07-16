638d3b3ba18cb72d75a88a70f31b09a70b0f2825
NikitaShashkin <137934427+NikitaShashkin@users.noreply.github.com>
2023-07-16 23:15:36 +0300

    Homework1JavaBasic

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Моя первая домашня работа!");
+
+        int number1 = 18;
+        int number2 = 9;
+        int number3 = number1 + number2;
+        int number4 = number1 - number2;
+        int number5 = number1 * number2;
+        int number6 = number1 / number2;
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(number4);
+        System.out.println(number5);
+        System.out.println(number6);
+    }
+}
